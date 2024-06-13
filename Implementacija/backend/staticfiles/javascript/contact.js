92442a8fe351f8d54f49313e981cdbb39d919977
Luka Kaludjerovic <162915572+LukaKaludjerovic@users.noreply.github.com>
2024-06-14 00:40:20 +0200

    Delete Implementacija/backend/staticfiles directory

--- Implementacija/backend/staticfiles/javascript/contact.js
+++
@@ -1,6 +0,0 @@
-//author: Luka Kaludjerovic 0041-2021
-function send_message(){
-    event.preventDefault();
-    alert("Message successfully sent. Please check your email for an answer. We aim to answer in no more than 24 hours.");
-    window.location.href="home";
-}
